7469a76cccc887c399fc05f7ac14ac95ab12e271
Alogenesis <nononosolo@hotmail.com>
2021-10-30 23:56:56 +0700

    Initial commit

---
+++ jsquiz.js
@@ -0,0 +1,143 @@
+// 1
+
+function correct1(){
+    document.getElementById("answer1").innerHTML = 'Correct'
+    let name = document.getElementById("answer1")
+    name.className = 'alert alert-success text-center'
+}
+
+function incorrect1(){
+    document.getElementById("answer1").innerHTML = 'Incorrect'
+    let name = document.getElementById("answer1")
+    name.className = 'alert alert-danger text-center'
+}
+
+// 2
+
+function correct2(){
+    document.getElementById("answer2").innerHTML = 'Correct'
+    let name = document.getElementById("answer2")
+    name.className = 'alert alert-success text-center'
+}
+
+function incorrect2(){
+    document.getElementById("answer2").innerHTML = 'Incorrect'
+    let name = document.getElementById("answer2")
+    name.className = 'alert alert-danger text-center'
+}
+
+// 3
+
+function correct3(){
+    document.getElementById("answer3").innerHTML = 'Correct'
+    let name = document.getElementById("answer3")
+    name.className = 'alert alert-success text-center'
+}
+
+function incorrect3(){
+    document.getElementById("answer3").innerHTML = 'Incorrect'
+    let name = document.getElementById("answer3")
+    name.className = 'alert alert-danger text-center'
+}
+
+// 4
+
+function correct4(){
+    document.getElementById("answer4").innerHTML = 'Correct'
+    let name = document.getElementById("answer4")
+    name.className = 'alert alert-success text-center'
+}
+
+function incorrect4(){
+    document.getElementById("answer4").innerHTML = 'Incorrect'
+    let name = document.getElementById("answer4")
+    name.className = 'alert alert-danger text-center'
+}
+
+// 5
+
+function correct5(){
+    document.getElementById("answer5").innerHTML = 'Correct'
+    let name = document.getElementById("answer5")
+    name.className = 'alert alert-success text-center'
+}
+
+function incorrect5(){
+    document.getElementById("answer5").innerHTML = 'Incorrect'
+    let name = document.getElementById("answer5")
+    name.className = 'alert alert-danger text-center'
+}
+
+// 6
+
+function correct6(){
+    document.getElementById("answer6").innerHTML = 'Correct'
+    let name = document.getElementById("answer6")
+    name.className = 'alert alert-success text-center'
+}
+
+function incorrect6(){
+    document.getElementById("answer6").innerHTML = 'Incorrect'
+    let name = document.getElementById("answer6")
+    name.className = 'alert alert-danger text-center'
+}
+
+
+
+
+// 7
+
+function correct7(){
+    document.getElementById("answer7").innerHTML = 'Correct'
+    let name = document.getElementById("answer7")
+    name.className = 'alert alert-success text-center'
+}
+
+function incorrect7(){
+    document.getElementById("answer7").innerHTML = 'Incorrect'
+    let name = document.getElementById("answer7")
+    name.className = 'alert alert-danger text-center'
+}
+
+// 8
+
+function correct8(){
+    document.getElementById("answer8").innerHTML = 'Correct'
+    let name = document.getElementById("answer8")
+    name.className = 'alert alert-success text-center'
+}
+
+function incorrect8(){
+    document.getElementById("answer8").innerHTML = 'Incorrect'
+    let name = document.getElementById("answer8")
+    name.className = 'alert alert-danger text-center'
+}
+
+// 9
+
+function correct9(){
+    document.getElementById("answer9").innerHTML = 'Correct'
+    let name = document.getElementById("answer9")
+    name.className = 'alert alert-success text-center'
+}
+
+function incorrect9(){
+    document.getElementById("answer9").innerHTML = 'Incorrect'
+    let name = document.getElementById("answer9")
+    name.className = 'alert alert-danger text-center'
+}
+
+// 10
+
+function correct10(){
+    document.getElementById("answer10").innerHTML = 'Correct'
+    let name = document.getElementById("answer10")
+    name.className = 'alert alert-success text-center'
+}
+
+function incorrect10(){
+    document.getElementById("answer10").innerHTML = 'Incorrect'
+    let name = document.getElementById("answer10")
+    name.className = 'alert alert-danger text-center'
+}
+
